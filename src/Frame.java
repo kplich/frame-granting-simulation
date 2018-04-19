fa7b098bdab1330f5a2433e564b76e5226ded7f7
Kamil Plich <kamilplich2@gmail.com>
2018-04-19 09:55:49 +0200

    basic scribbles - first constructor

---
+++ src/Frame.java
@@ -0,0 +1,25 @@
+public class Frame<P extends Page> {
+	int frameIndex;
+	P pageGiven;
+
+	public Frame(int frameIndex, P pageGiven) {
+		this.frameIndex = frameIndex;
+		this.pageGiven = pageGiven;
+	}
+
+	public int getFrameIndex() {
+		return frameIndex;
+	}
+
+	public void setFrameIndex(int frameIndex) {
+		this.frameIndex = frameIndex;
+	}
+
+	public P getPageGiven() {
+		return pageGiven;
+	}
+
+	public void setPageGiven(P pageGiven) {
+		this.pageGiven = pageGiven;
+	}
+}
--- src/Process.java
+++ src/Process.java
@@ -1,3 +1,27 @@
-public class Process {
+import java.util.*;
 
+public class Process {
+	private int size;
+
+	private ArrayList<Frame<LRUPage>> frameTable = new ArrayList<>();
+	private ArrayList<LRUPage> pageTable = new ArrayList<>();
+
+	private LinkedList<LRUPage> requestQueue = new LinkedList<>();
+
+	public Process(int simulationSize, int pageNumber, int frameNumber) {
+		for(int i = 0; i<pageNumber; ++i) {
+			pageTable.add(new LRUPage(i, -1));
+		}
+
+		for(int i = 0; i<frameNumber; ++i) {
+			frameTable.add(new Frame<>(i, null));
+		}
+
+		Random rng = new Random();
+
+		//losowo generateRequests();
+		for(int i = 0; i<simulationSize; ++i) {
+			requestQueue.add(pageTable.get(rng.nextInt(pageNumber)));
+		}
+	}
 }
